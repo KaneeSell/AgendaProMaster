1b0cd228b2cd0b638d0a1fcbbfc17e7ac24abfba
Daniel Henrique Sell <kanee.sell@gmail.com>
2024-11-25 01:24:32 -0300

    visual pagina

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-//Declarando constantes
+// Ao carregar a pagina
+verificarLocalStorage()
+maxCaracterNome(false, localStorage.getItem('maxNome'))
+maxCaracterDescricao(false, localStorage.getItem('maxDescricao'))
+// Declarando constantes
 const barNavegacao = document.getElementById('barNavegacao');
 const divBemVindo = document.getElementById('divBemVindo')
 const divAgenda = document.getElementById('divAgenda')
@@ -7,7 +11,15 @@ const liBemVindo = document.getElementsByClassName('nav-link')[0]
 const liAgenda = document.getElementsByClassName('nav-link')[1]
 const liConfiguracoes = document.getElementsByClassName('nav-link')[2]
 
-//Funções
+// Funções
+function verificarLocalStorage(){
+    const maxNome = localStorage.getItem('maxNome')
+    const maxDescricao = localStorage.getItem('maxDescricao')
+    if(maxNome == undefined && maxDescricao == undefined){
+        localStorage.setItem('maxNome', 30)
+        localStorage.setItem('maxDescricao', 300)
+    }
+}
 function hideBarNavegacao(...elementos){
     const collapse = new bootstrap.Collapse(barNavegacao);
     collapse.toggle();
@@ -38,4 +50,58 @@ function linkConfiguracoes(){
     divAgenda.style.display = 'none'
     divBemVindo.style.display = 'none'
     divConfiguracoes.style.display = 'flex'
+}
+// Funções de configurações
+function maxCaracterDescricao(e = false, maxDescricao = false){
+    if(e){
+        e.preventDefault();
+    }
+    const inputMaxCaracterDescricao = document.getElementById('customRange2')
+    const labelmaxCaracterDescricao = document.getElementById('maxCaracterDescricao')
+    if(maxDescricao){
+        labelmaxCaracterDescricao.innerText = `Máximo caracteres Descrição ${maxDescricao}`
+        inputMaxCaracterDescricao.value = maxDescricao
+    } else{
+        labelmaxCaracterDescricao.innerText = `Máximo caracteres Descrição ${inputMaxCaracterDescricao.value}`
+    }
+}
+function maxCaracterNome(e = false, maxNome = false){
+    if(e){
+        e.preventDefault();
+    }
+    const inputMaxCaracterNome = document.getElementById('customRange3')
+    const labelmaxCaracterNome = document.getElementById('maxCaracterNome')
+    if(maxNome){
+    labelmaxCaracterNome.innerText = `Máximo caracteres nome ${maxNome}`
+    inputMaxCaracterNome.value = maxNome
+    } else{
+        labelmaxCaracterNome.innerText = `Máximo caracteres nome ${inputMaxCaracterNome.value}`
+    }
+}
+// Tooltip and popover demos
+document.querySelectorAll('.tooltip-demo')
+.forEach(tooltip => {
+  new bootstrap.Tooltip(tooltip, {
+    selector: '[data-bs-toggle="tooltip"]'
+  })
+})
+
+document.querySelectorAll('[data-bs-toggle="popover"]')
+.forEach(popover => {
+  new bootstrap.Popover(popover)
+})
+
+document.querySelectorAll('.toast')
+.forEach(toastNode => {
+  const toast = new bootstrap.Toast(toastNode, {
+    autohide: false
+  })
+
+  toast.show()
+})
+// Funções Agenda
+function visualizarEvento(e = false){
+    if(e){
+        e.preventDefault();
+    }
 }
